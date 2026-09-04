b8799b34fc72f6ba658d008703e0599021cb235b
João Pedro Jaureguy <jaureguy.jau@gmail.com>
2026-09-04 17:37:51 -0300

    Função 01 aperfeiçoada

--- funcao-1.py
+++ funcao-1.py
@@ -1,15 +1,12 @@
 ##Ler um valor e através de uma função dar true se é par, false caso seja ímpar
 
 def ehPar(a):
-    if a%2==0:
-        return True
-    else:
-        return False
+    return a%2==0
 
 
 x = int(input("Digite um valor: "))
 
-if ehPar(x)==True:
+if ehPar(x):
     print("Este valor eh par")
 else:
     print("Este valor eh impar")
