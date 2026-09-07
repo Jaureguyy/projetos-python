5849e03ed21a52d205a53f433ede68aaf296fb87
João Pedro Jaureguy <jaureguy.jau@gmail.com>
2026-09-07 00:26:43 -0300

    Leitura e soma de matriz refeita. Utilizando um único loop para ambas ações

--- matrizes-1.py
+++ matrizes-1.py
@@ -1,15 +1,12 @@
 ##Ler 'n' linhas e 'm' colunas de uma matriz, após isso mostrar a soma de todos os elementos.
 
 linhas, colunas = map(int, input("Digite o numero de linhas e colunas da matriz: ").split())
-matriz=[]
+matriz=[] ##Definir a matriz aqui é desnecessário, em vista que a linha da matriz é lida e logo em seguida descartada
 soma=0
 
 for i in range(linhas):
     linha = list(map(int,input(f"Digite os valores da linha {i}:").split()))
     matriz.append(linha)
-
-for i in range(linhas):
-    for j in range(colunas):
-        soma+=matriz[i][j]
+    soma+= sum(linha)
 
 print("Soma da matriz:", soma)
