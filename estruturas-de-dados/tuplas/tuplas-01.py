a4861aa0b06589595f69bb40731b37a8af7dd59f
João Pedro Jaureguy <jaureguy.jau@gmail.com>
2026-09-11 15:16:22 -0300

    Conceitos de uma tupla

---
+++ estruturas-de-dados/tuplas/tuplas-01.py
@@ -0,0 +1,8 @@
+#Diferentemente das listas, as tuplas possuem parênteses ao invés de colchetes:
+###lista = [1, 2, 3, 4, 5]
+###tupla = (1, 2, 3, 4, 5)
+
+#Outra característica principal é que as tuplas são imutáveis, tanto nos seus valores quanto em tamanho/posição.
+#Para fazer alterações, é necessário criar uma nova tupla com base na antiga:
+###tupla = (1, 2, 3)
+###nova_tupla = tupla + (4,)             -> a vírgula após o 4 é necessária para que o python entenda que se trata de uma tupla
