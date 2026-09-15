e33a18fd445675ca3f0341dcb06609bca0205abe
João Pedro Jaureguy <jaureguy.jau@gmail.com>
2026-09-14 22:16:51 -0300

    Primeiro dicionario utilizando um produto como exemplo

---
+++ estruturas-de-dados/dicionarios/dicionario-01.py
@@ -0,0 +1,15 @@
+"""
+-Dicionário = { chave : valor, chave2 : valor2, chave3 : valor#}
+
+-Métodos nos dicionários:
+
+1°: .keys(): retorna todas as chaves do dicionário
+2°: .values(): retorna todos os valores do dicionário
+3°: .items(): retorna os pares -> chave : valor
+4°: .get(): acessa a chave de forma segura mesmo que ela não exista
+
+-Percorrendo um dicionário com for:
+
+for chave, valor in pessoa.item():                 ###isto fica semelhante ao desempacotamento de tuplas
+    print(f"{chave}: {valor}")
+"""
---
+++ estruturas-de-dados/dicionarios/dicionario-02.py
@@ -0,0 +1,24 @@
+#Criar um dicionário para um produto, contendo informações como: nome, preço e quantidade. 
+#Será sem input, apenas irei criar e após isso apresentar os valores de forma separada e posteriormente os pares
+
+
+produto = {
+    "nome" : "Tanque de Guerra",
+    "preco" : 50.000,
+    "quantidade" : 1
+}
+
+#Apresentando os valores de forma separada
+print(f"Nome do produto: {produto.get("nome")}")
+print(f"Valor do produto: {produto.get("preco")}")
+print(f"Quantidade de produtos: {produto.get("quantidade")}")
+
+print(f"\n{'-'*30}\n")
+
+#Utilizando get em uma chave inexistente
+print(f"Endereco de entrega: {produto.get('endereco_de_entrega', 'rua Brasil')}")
+print(f"\n{'-'*30}\n")
+
+#Apresentando os pares
+for chave, valor in produto.items():            ##Neste caso o endereço de entrega não existe pois ele não foi add no dicionario 
+    print(f"{chave}: {valor}")
