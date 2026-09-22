48697a1f7df90be4c699d4ff668cc49391af04ee
João Pedro Jaureguy <jaureguy.jau@gmail.com>
2026-09-22 20:16:52 -0300

    Explicações dentro do código + testes finais do FUNCIONAMENTO

--- Calculadora/main.py
+++ Calculadora/main.py
@@ -1,5 +1,8 @@
 """
-Com o dispatcher criado, agora basta inicializar com 2 valores e 1 operação. Inicialmente o primeiro valor será inicializado junto do segundo, porém posteriormente ele se tornará o resultado e será reaproveitado. Dentro de um while será viável"""
+Com o dispatcher criado, agora basta inicializar com 2 valores e 1 operação. Inicialmente o primeiro valor será inicializado junto do segundo, porém posteriormente ele se tornará o resultado e será reaproveitado. Dentro de um while será viável
+
+Atualmente só existem as operações: '+' ; '-' ; '*' e '/' (divisão)
+"""
 
 ## Import do dispatcher
 
@@ -13,13 +16,20 @@ valor1, operacao, valor2 = calculo.split()
 valor1 = int(valor1)
 valor2 = int(valor2)
 
+
+## Loop para rodar a calculadora
+
 while operacao != "X":
-    resultado = dispatcher(valor1, valor2, operacao)
+    #Calcula o resultado chamando o dispatcher
+    resultado = dispatcher(valor1, valor2, operacao)    
     print(f"Resultado: {resultado}\n")
+    #Seguimos com o calculo no mesmo formato porém agora com RESULTADO -> RESULTADO OPERACAO RESULTADO
     novo_calculo = input(f"Digite a continuacao do calculo ou 'X' para cancelar:\n->{resultado} ")
 
+    ##Caso seja digitado um 'X' para o loop
     if novo_calculo=="X":
         break
+    #Reestabelece alguns valores como valor1 (derivado de resultado) e valor2 como int
     operacao, valor2 = novo_calculo.split()
     valor1 = resultado
     valor2 = int(valor2)
